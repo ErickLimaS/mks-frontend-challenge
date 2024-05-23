f2074bc2f345b0f727ca59c54314ca92b4d68c9e
Erick Lima <ericklima63@gmail.com>
2024-05-23 15:59:24 -0300

    style(animation): Add loading animation on ProductsGrid
    Updated ProductsGrid component with new animation and loading state, and added target="es2015" to tsconfig.json

--- app/components/ProductsGrid/index.tsx
+++ app/components/ProductsGrid/index.tsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import ProductCard from '../ProductCard'
 import { AnimatePresence, motion } from 'framer-motion'
+import simulateRange from '@/app/lib/simulateRange'
 
 const opacityStaggerAnimation = {
     initial: {
@@ -12,8 +13,12 @@ const opacityStaggerAnimation = {
     animate: {
         opacity: 1,
         transition: {
+            duration: 0.4,
             staggerChildren: 0.120,
         },
+    },
+    exit: {
+        opacity: 0,
     },
 }
 
@@ -35,8 +40,31 @@ function ProductsGrid() {
     }
 
     return (
-        <AnimatePresence>
-            {isLoading == false && (
+        <AnimatePresence mode='wait'>
+            {isLoading ? (
+
+                <motion.ul
+                    variants={opacityStaggerAnimation}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    className="max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg m-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 justify-items-center"
+                >
+
+                    {simulateRange(8).map((index) => (
+                        <motion.li
+                            key={index}
+                            initial={{ opacity: 0.6 }}
+                            animate={{ opacity: [0.6, 1, 0.6] }}
+                            transition={{ repeat: Infinity, duration: 1.5 }}
+                            className="min-h-[355px] min-w-[245px] md:min-w-[202px] lg:min-w-[245px] bg-black/35 rounded-lg"
+                        />
+                    ))}
+
+                </motion.ul>
+
+            ) : (
+
                 <motion.ul
                     variants={opacityStaggerAnimation}
                     initial="initial"
@@ -47,7 +75,7 @@ function ProductsGrid() {
                     {(data.products as ProductType[]).map(item => (
 
                         <motion.li
-                            key={item.id}
+                            key={item.name}
                             variants={opacityStaggerAnimation}
                             className="w-8/12 sm:w-4/5 md:w-full max-h-[365px]"
                         >
@@ -59,8 +87,9 @@ function ProductsGrid() {
                     ))}
 
                 </motion.ul>
+
             )}
-        </AnimatePresence>
+        </AnimatePresence >
     )
 }
 
