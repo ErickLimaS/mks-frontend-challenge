0fb131f81aaadea9432880bbd0db5a6e3e7044a2
Erick Lima <ericklima63@gmail.com>
2024-05-24 14:32:31 -0300

    test(fix, add): Add close Checkout and Fix FramerMotion Animations on test
    Updated CheckoutSidebar test to disable Framer Motion animations and added new test for closing sidebar when Close Button is clicked

--- __tests__/components/checkoutSidebar.test.tsx
+++ __tests__/components/checkoutSidebar.test.tsx
@@ -5,14 +5,11 @@ import StoreProvider from "@/app/StoreProvider"
 import ReactQueryProvider from "@/app/lib/ReactQuery/Provider";
 import userEvent from '@testing-library/user-event'
 import { render, screen } from '@testing-library/react'
+import { MotionGlobalConfig } from "framer-motion"
+
+MotionGlobalConfig.skipAnimations = true
 
 describe("Checkout Sidebar", () => {
-
-    // *** ATTENTION ***
-    // 
-    // FRAMER MOTION CAN'T be MOCKED
-    // so it will FAIL the TEST to be VISIBLE
-    // 
 
     it("gets open when Header Cart Button is clicked", async () => {
 
@@ -44,8 +41,50 @@ describe("Checkout Sidebar", () => {
 
         const checkoutSidebarHeadindText = screen.getByText("Carrinho de compras")
 
-        // ATTENTION: Framer Motion can not be mocked, so it will almost everytime show as Not Visible
         expect(checkoutSidebarHeadindText).toBeVisible()
+
+    })
+
+    it("closes when Close Button(X) is clicked", async () => {
+
+        const user = userEvent.setup()
+
+        render(
+            <StoreProvider>
+
+                <ReactQueryProvider>
+
+                    <StyledComponentsRegistry>
+                        <Layout>
+                            <></>
+                        </Layout>
+                    </StyledComponentsRegistry>
+
+                </ReactQueryProvider> 
+
+            </StoreProvider>
+        )
+
+        const headerCartBtn = screen.getByRole("button", { name: "0" }) // Name is 0 due to quantity of products on cart
+
+        // Check if cart btn is on screen
+        expect(headerCartBtn).toBeVisible()
+
+        // This button being clicked should change the Checkout State to True (visible)
+        await user.click(headerCartBtn)
+
+        const checkoutSidebarHeadindText = screen.getByText("Carrinho de compras")
+
+        expect(checkoutSidebarHeadindText).toBeVisible()
+
+        // Close button on Checkout
+        const closeBtn = screen.getByText('X') 
+        expect(closeBtn).toBeVisible()
+
+        // Close Checkout, Changing State to False
+        await user.click(closeBtn)
+
+        expect(checkoutSidebarHeadindText).not.toBeVisible()
 
     })
 
